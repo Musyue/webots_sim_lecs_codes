644c4ac4fa3c00057a11e56367746a70a1ec3a3f
Musyue <cboy@mail.ustc.edu.cn>
2021-01-26 11:48:25 +0800

    1,update lec5 code

---
+++ webots_lec_5/controllers/four_wheel_sim/build/release/four_wheel_sim.d
@@ -0,0 +1,6 @@
+build/release/four_wheel_sim.o: four_wheel_sim.cpp \
+  /Applications/Webots.app/include/controller/cpp/webots/DistanceSensor.hpp \
+  /Applications/Webots.app/include/controller/cpp/webots/Device.hpp \
+  /Applications/Webots.app/include/controller/cpp/webots/../../c/webots/types.h \
+  /Applications/Webots.app/include/controller/cpp/webots/Motor.hpp \
+  /Applications/Webots.app/include/controller/cpp/webots/Robot.hpp
